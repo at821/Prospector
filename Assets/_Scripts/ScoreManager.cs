bf5de28b2c992b812d8e4c7123d7566512c7671a
at821 <36107231+at821@users.noreply.github.com>
2018-11-20 03:54:06 -0500

    784

---
+++ Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eScoreEvent{
+    draw,
+    mine,
+    mindGold,
+    gameWin,
+    gameLoss
+}
+
+public class ScoreManager : MonoBehaviour {
+
+	static private ScoreManager S
+
+    
+   
+}
